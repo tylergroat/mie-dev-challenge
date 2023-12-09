94bf683d1e04bd1943039b03d200a1d07ee83ea5
Tyler Groat <gh5295@wayne.edu>
2023-12-09 15:11:12 -0500

    Set up game_session.js and add-game-session.ejs

--- routes/game_session.js
+++ routes/game_session.js
@@ -1,14 +1,42 @@
+// routes/game_session.js
+
 module.exports = {
-	getAdd: (req, res) => {
-		res.render('add-game.ejs', {
-			title: 'Board Games | Add game'
-		});
-	},
-	postAdd: (req, res) => {
-		console.log(req.body);
+  getAdd: (req, res) => {
+    // Fetch available games to populate the dropdown
+    pool.getConnection()
+      .then(connection => {
+        return connection.query('SELECT game_id, title FROM Games');
+      })
+      .then(games => {
+        res.render('add-game-session.ejs', {
+          title: 'Board Games | Add Game Session',
+          games,
+        });
+      })
+      .catch(err => {
+        console.error('Error fetching games for session add:', err);
+        res.redirect('/');
+      });
+  },
 
-		// TODO db.query to insert game-playing session
-
-		res.redirect('/');
-	}
+  // Process the form submission for adding a game-playing session
+  postAdd: (req, res) => {
+    const { gameId, datePlayed, attendees, comments } = req.body;
+    pool.getConnection()
+      .then(connection => {
+        // Insert a new game session into the 'GameSessions' table
+        return connection.query(
+          'INSERT INTO GameSessions (game_id, date_played, attendees, comments) VALUES (?, ?, ?, ?)',
+          [gameId, datePlayed, attendees, comments]
+        );
+      })
+      .then(() => {
+        console.log('Game session added successfully');
+        res.redirect('/'); // Redirect to the home page after adding a game session successfully
+      })
+      .catch(err => {
+        console.error('Error adding game session:', err);
+        res.redirect('/add-game-session'); // Redirect to the add-game-session page in case of an error
+      });
+  },
 };
