40e028eff8c920263854c158e336416c55f73931
Tyler Groat <gh5295@wayne.edu>
2023-12-10 21:12:08 -0500

    Passing title variable in index.js

--- routes/index.js
+++ routes/index.js
@@ -2,18 +2,17 @@
 
 module.exports = {
 	getHomePage: (req, res) => {
-
 		// Use the connection pool to query the database for games
 		pool.getConnection()
 			.then(connection => {
 				return connection.query('SELECT * FROM Games');
 			})
 			.then(games => {
-				res.render('index', { games }); // Pass the fetched games to the EJS template
+				res.render('index', { games, title: 'Board Games' });
 			})
 			.catch(err => {
 				console.error('Error fetching games:', err);
-				res.render('index', { games: [] }); // Render the template with an empty array if an error occurs
+				res.render('index', { games: [], title: 'Board Games' });
 			});
 	}
 };
